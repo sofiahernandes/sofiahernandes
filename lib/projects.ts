b9bbec32b621477e88fd8bc6c8ebfedb7f4e4449
Sofia B. Hernandes <sofiahernandes.dev@gmail.com>
2025-08-11 16:34:51 -0300

    update: project upload

--- lib/projects.ts
+++ lib/projects.ts
@@ -4,20 +4,20 @@ export const projects = [
         description: "Next.js Ecommerce Storefront of planners and tools designed to help you heal, grow, and thrive!",
         image: "/images/glow-screenshot.png",
         link: "https://github.com/sofiahernandes/glow-global",
-        tags: ["Next", "TypeScript", "Tailwind", "Integration"]
+        tags: ["Next.js", "TypeScript", "Tailwind", "Integration"]
     },
     {
         title: "Notion Clone",
         description: "A full-featured Next.js clone of Notion built with Blocknote rich-text editor.",
         image: "/images/notion-screenshot.png",
         link: "https://github.com/sofiahernandes/notion-clone",
-        tags: ["Next", "React", "TypeScript", "Tailwind", "Clerk", "Blocknote"],
+        tags: ["Next.js", "React", "TypeScript", "Tailwind", "Clerk", "Blocknote"],
     },
     {
-        title: "Tech Jobs App",
-        description: "A job search app built with React Native, powered by JSearch API and Google Auth.",
-        image: "/images/tech-screenshot.png",
-        link: "https://github.com/sofiahernandes/tech-jobz-app",
-        tags: ["React Native", "Expo", "Axios", "Material UI", "Google Authentication"],
+        title: "Bookie Chatbot",
+        description: "A Gemini API powered chatbot that helps you find books based on your preferences.",
+        image: "/images/bookie-screenshot.jpg",
+        link: "https://github.com/sofiahernandes/gemini-book-chatbot",
+        tags: ["Chatbot", "Gemini API", "AI", "Next.js", "TypeScript", "Next Auth"],
     },
 ]
